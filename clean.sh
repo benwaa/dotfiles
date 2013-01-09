bc1879abe53ddbaafb129016a53c3e52897f2b8c
Ben <corda@Vine-3.local>
2013-01-09 11:49:08 -0500

    add clean.sh

---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+find . -name ".AppleDouble" -exec rm -rvf {} \;
+find . -name ".DS_Store" -exec rm -vf {} \;
+find . -name "*~" -exec rm -vf {} \;
+find . -name "*~" -exec rm -vf {} \;
+find . -name "#*#" -exec rm -vf {} \;
+find . -name "*.pyo" -exec rm -vf {} \;
+find . -name ".#*" -exec rm -vf {} \;
+find . -name "Thumbs.db" -exec rm -vf {} \;
