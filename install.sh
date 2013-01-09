fc65e54811379cd4122802ebdf63378b1e60bcee
Ben <corda@Vine-3.local>
2013-01-09 11:50:32 -0500

    update install to include clean.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ git pull && git submodule init && git submodule update && git submodule status
 ln -s `pwd`/oh-my-zsh ~/.oh-my-zsh
 ln -s `pwd`/.zshrc ~/.zshrc
 ln -s `pwd`/.zshenv ~/.zshenv
+ln -s `pwd`/clean.sh ~/clean.sh
